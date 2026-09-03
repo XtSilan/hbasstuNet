53af8548fe7b8be2b2cab1007eda6d69932dd90b
XtSilan <xt2890207721@163.com>
2026-09-03 20:09:05 +0800

    改进程序更新替换方式

--- updater_windows.go
+++ updater_windows.go
@@ -10,7 +10,7 @@ import (
 )
 
 func launchUpdater(updatePath, executable string, pid int) error {
-	command := fmt.Sprintf("$p=Get-Process -Id %s -ErrorAction SilentlyContinue; if($p){Wait-Process -Id %s}; Start-Sleep -Milliseconds 300; Move-Item -Force -LiteralPath %s -Destination %s; Start-Process -FilePath %s", strconv.Itoa(pid), strconv.Itoa(pid), escapePS(updatePath), escapePS(executable), escapePS(executable))
+	command := fmt.Sprintf("$p=Get-Process -Id %s -ErrorAction SilentlyContinue; if($p){Wait-Process -Id %s}; Start-Sleep -Milliseconds 300; $u=%s; $e=%s; try {[System.IO.File]::Replace($u,$e,$null,$true)} catch {Move-Item -Force -LiteralPath $u -Destination $e}; Start-Process -FilePath $e", strconv.Itoa(pid), strconv.Itoa(pid), escapePS(updatePath), escapePS(executable))
 	cmd := exec.Command("powershell.exe", "-NoProfile", "-NonInteractive", "-WindowStyle", "Hidden", "-Command", command)
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true, CreationFlags: 0x08000000}
 	if err := cmd.Start(); err != nil {
